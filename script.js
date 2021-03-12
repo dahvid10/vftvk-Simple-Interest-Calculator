c193f60e4f4536a26e14ad5941c89da66de913cb
David Mkoji <david.mkoji@gmail.com>
2021-03-11 21:10:16 -0500

    Commiting changes to index.html, script.js, and style.css for Simple Interest Calculator app

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute(){//compute the total amount earned with simple interest
+    var principal = document.getElementById("principal").value;//capture principal value 
+    var rate = document.getElementById("rate").value;//capture rate value
+    var years = document.getElementById("years").value;//capture years value
+    var interest = principal * years * rate / 100;//compute the total amount earned with simple interest
+    var this_year = new Date().getFullYear();//capture current year
+    var tbd = this_year + parseInt(years);//find actual year used to calculate simple interest
+    if( check_principal() == true)   {//if principal value is valid, display results  
+        document.getElementById("result").innerHTML = 
+        "&emsp;" + "If you deposit <mark>" + principal + "</mark>,<br>" +  
+        "&emsp;" + "at an interest rate of <mark>" + rate + "%</mark>.<br>" + 
+        "&emsp;" + "You will receive an amount of <mark>" + interest + "</mark>, <br>" + 
+        "&emsp;" + "in the year <mark>" + tbd + "</mark><br><br>"
+    }    
+}
+
+function change_rate(){//display rate from slider
+    var slider_value = document.getElementById("rate").value;
+    document.getElementById("change").innerHTML = slider_value;
+}
+   
+function drop_down_years(){//create drop down menu for number of years
+    var years = document.getElementById("years"); 
+    var options = "";
+    for(i=1;i<=10;i++){
+        options += "<option value=" + i + ">" + i + "</option>";        
+    }   
+    years.innerHTML = options;       
+}
+
+window.onload = drop_down_years;//load drop down menu when window loads
+
+function check_principal(){//check if principal value is valid (non-zero and positive)
+    var p = document.getElementById("principal");//capture principal amount
+
+    if(p.value == 0 || p.value < 0){//yield alert if principal amount is invalid
+        alert("Enter a positive number");
+        p.focus();
+        document.getElementById("result").innerHTML = "";
+        return false;
+    }
+    return true;
     
 }
-        
+
